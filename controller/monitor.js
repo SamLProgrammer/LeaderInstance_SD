ce6d0e1b317af7678e10490c59e80e789d609062
Samuel López <samuel.lopez@uptc.edu.co>
2021-10-01 15:53:47 -0500

    stillstupid

--- controller/monitor.js
+++ controller/monitor.js
@@ -8,12 +8,13 @@ const joinToInstances = () => {
     fs.readFile('../ip_list.txt', function (err, data) {
         if (err) throw err;
         var array = data.toString().split("\n");
-        for (i in array) {
+        for (let i = 0; i < array.length - 1; i++) {
             nodes_ip_list.push(array[i]);
         }
-        for(let i = 0; i < array.length - 1 ; i++) {
-        console.log('===========')
+        for (let i = 0; i < nodes_ip_list.length; i++) {
+            console.log('ip : ' + nodes_ip_list[i])
         }
+        console.log('===========')
     });
 }
 
