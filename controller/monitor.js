a51bcd5431aa4969de18880b6f46d7d5b5489ac8
Samuel López <samuel.lopez@uptc.edu.co>
2021-10-01 18:43:44 -0500

    king of logs

--- controller/monitor.js
+++ controller/monitor.js
@@ -8,9 +8,9 @@ let leader_flag = false;
 
 const newJoin = (req, res) => {
     console.log(req.query.ip)
-    connections_list.push({ip: req.query.ip, leader: false})
-    res.send({leader: leader_flag})
-    for(let i = 0; i < connections_list.length; i ++) {
+    connections_list.push({ ip: req.query.ip, leader: false })
+    res.send({ leader: leader_flag })
+    for (let i = 0; i < connections_list.length; i++) {
         console.log(connections_list[i]);
     }
 }
@@ -37,17 +37,17 @@ const getIp = () => {
             axios.get('http://' + nodes_ip_list[i] + ':5000/newJoin?ip=' +
                 local_ip).then(function (response) {
                     console.log('I got response: ' + i)
-                    const object = {ip: nodes_ip_list[i], leader: response.data.leader}
+                    const object = { ip: nodes_ip_list[i], leader: response.data.leader }
                     connections_list.push(object);
+                    if(i == nodes_ip_list.length-1) {
+                        for(let i = 0; connections_list.length; i++) {
+                            console.log('nice : ' + connections_list[i])
+                        }
+                    }
                 }).catch(err => {
                     console.log('err')
                 });
         }
-        console.log('connections')
-        for(let i = 0; i < connections_list.length; i++) {
-            console.log(connections_list[i]);
-        }
-        console.log('=============')
     });
     ls.stderr.on('data', (data) => {
         console.error(`stderr: ${data}`);
