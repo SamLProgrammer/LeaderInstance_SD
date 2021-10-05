6eee60632e1bdf2e2f01c06d071ffd4920d64f08
drox13 <dario.baron@uptc.edu.co>
2021-10-04 19:21:39 -0500

    se agrego la tabla y el label para mostrar el lider y los metodos en el cliente para actualizar

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,22 @@ window.onload = () => {
 }
 
 const socket = io();
+var tableBodyElement = document.getElementById('table-body');
+var leader = document.getElementById('leader');
+
+//este es el metodo que escucha cuando llega una nueva instancia llega un objeto
+
+socket.on("newInstance", (info) => {
+    let cell = '<tr><th scope="row">' + info.number + '</th><td>' + info.ip + '</td><td>' + info.port + '</td><td>' + "otra info" + '</td></tr>';
+	let row = document.createElement('TR');
+	row.innerHTML = cell;
+	tableBodyElement.appendChild(row);
+});
+
+//este metodo actualiza quien es el lider como dato
+socket.on('newleader', (leader)=>{
+    leader.innerHTML = leader;
+});
 
 socket.on('free', (arg) => { 
 	new_instance_button.style.display = 'block';
